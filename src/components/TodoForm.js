dd1ef0209354005857afcdd6d7d788af5b8a336b
Saad Mansoor <daasmn@gmail.com>
2023-02-16 19:07:23 -0500

    Final Styling and Edit Capabilities added.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,42 +1,62 @@
 import React, { useState, useEffect, useRef } from "react";
 
 function TodoForm(props) {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
 
+  const inputRef = useRef(null);
 
-    const inputRef = useRef(null)
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
-    useEffect(() => {
-        inputRef.current.focus()
-    })
-
-    const handleChange = e => {
-        setInput(e.target.value);
-    }
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
 
   const handleSubmit = e => {
-      e.preventDefault();
+    e.preventDefault();
 
     props.onSubmit({
-       id: Math.floor(Math.random() * 10000),
+      id: Math.floor(Math.random() * 10000),
       text: input
     });
 
-    setInput('');
+    setInput("");
   };
 
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Add a To-do"
-        value={input}
-        name="text"
-        className="todo-input"
-        onChange={handleChange}
-        ref={inputRef}
-      />
-      <button className="todo-button" type="">Add a To-Do</button>
+      {props.edit ? (
+        <>
+          <input
+            type="text"
+            placeholder="Edit Item"
+            value={input}
+            name="text"
+            className="todo-input edit"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button edit" type="">
+            Update
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            placeholder="Add a To-do"
+            value={input}
+            name="text"
+            className="todo-input"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button" type="">
+            Add a To-Do
+          </button>
+        </>
+      )}
     </form>
   );
 }
